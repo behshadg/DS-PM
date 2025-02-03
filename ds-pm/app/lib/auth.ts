40bfb699325acbfc078ba63d55c2480332fb8df3
behshadg <ghassemiben@gmail.com>
2025-02-02 21:18:59 -0500

    minor x7

--- ds-pm/app/lib/auth.ts
+++ ds-pm/app/lib/auth.ts
@@ -1,7 +1,7 @@
 
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db";
 import { compare } from "bcryptjs";
 import { LoginSchema } from "./schema";
